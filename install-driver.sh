d1786f725a9773517459f943450f55e9e1d1cb67
lucien.minot <lucien.minot@medmen.com>
2018-09-24 09:27:11 -0700

    Pipeline tests

--- install-driver.sh
+++ install-driver.sh
@@ -2,6 +2,9 @@
 # download and install latest geckodriver for linux or mac.
 # required for selenium to drive a firefox browser.
 
+sudo apt-get install jq
+chmod +x jq
+
 json=$(curl -s https://api.github.com/repos/mozilla/geckodriver/releases/latest)
 if [[ $(uname) == "Darwin" ]]; then
     url=$(echo "$json" | jq -r '.assets[].browser_download_url | select(contains("macos"))')
@@ -13,4 +16,4 @@ else
 fi
 curl -s -L "$url" | tar -xz
 chmod +x geckodriver
-echo "installed geckodriver binary in $install_dir"
+echo "installed geckodriver binary in"
