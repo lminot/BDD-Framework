fa7d7083077f5df7a409a4f73bedc6c0c7c6d2f6
lucien.minot <lucien.minot@medmen.com>
2018-09-24 14:05:37 -0700

    last try with firefox driver

--- chromedriver-install.sh
+++ chromedriver-install.sh
@@ -2,7 +2,7 @@
 # download and install latest chromedriver for linux or mac.
 # required for selenium to drive a Chrome browser.
 
-version=$(wget -qO- https://chromedriver.storage.googleapis.com/LATEST_RELEASE)
+version=$(curl -s https://chromedriver.storage.googleapis.com/LATEST_RELEASE)
 if [[ $(uname) == "Darwin" ]]; then
     url=https://chromedriver.storage.googleapis.com/$version/chromedriver_mac64.zip
 elif [[ $(uname) == "Linux" ]]; then
