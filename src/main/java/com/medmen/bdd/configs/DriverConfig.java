e2856a61bb28bc3b0c9b260bb61187301f77c12d
lucien.minot <lucien.minot@medmen.com>
2018-12-13 11:30:10 -0800

    Fixing driver config for firefox to clean up logging

--- src/main/java/com/medmen/bdd/configs/DriverConfig.java
+++ src/main/java/com/medmen/bdd/configs/DriverConfig.java
@@ -8,6 +8,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxDriverLogLevel;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.phantomjs.PhantomJSDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -17,118 +18,124 @@ import org.springframework.util.ObjectUtils;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
 
 public class DriverConfig {
 
-  static final Logger logger = LogManager.getLogger(DriverConfig.class);
+    static final Logger logger = LogManager.getLogger(DriverConfig.class);
 
-  private static long DEFAULT_WAIT = 20;
-  private static String GRID_URL = "http://localhost:4444/wd/hub";
-  private static String driverName = System.getProperty("browser", "firefox");
-  private static String browserLocation = System.getProperty("browserLocation", "local");
-  private static String isHeadless = System.getProperty("headless", "true");
+    private static long DEFAULT_WAIT = 20;
+    private static String GRID_URL = "http://localhost:4444/wd/hub";
+    private static String driverName = System.getProperty("browser", "firefox");
+    private static String browserLocation = System.getProperty("browserLocation", "local");
+    private static String isHeadless = System.getProperty("headless", "true");
 
-  private static WebDriver driver;
+    private static WebDriver driver;
 
-  public static WebDriver getDriver() {
-    if (!ObjectUtils.isEmpty(driver)) {
-      return driver;
+    public static WebDriver getDriver() {
+        if (!ObjectUtils.isEmpty(driver)) {
+            return driver;
+        }
+
+        switch (browserLocation) {
+            case "remote":
+                try {
+                    driver = new RemoteWebDriver(new URL(GRID_URL), setDesiredCapabilities());
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                }
+                break;
+
+            case "local":
+                driver = chooseDriver(setDesiredCapabilities());
+                break;
+
+            default:
+                logger.error("Exception: Invalid environment " + browserLocation);
+        }
+        return manageDriver(driver);
     }
 
-    switch (browserLocation) {
-      case "remote":
-        try {
-          driver = new RemoteWebDriver(new URL(GRID_URL), setDesiredCapabilities());
-        } catch (MalformedURLException e) {
-          e.printStackTrace();
-        }
-        break;
-
-      case "local":
-        driver = chooseDriver(setDesiredCapabilities());
-        break;
-
-      default:
-        logger.error("Exception: Invalid environment " + browserLocation);
-    }
-    return manageDriver(driver);
-  }
-
-  private static WebDriver manageDriver(WebDriver driver) {
-    driver.manage().timeouts().setScriptTimeout(DEFAULT_WAIT, TimeUnit.SECONDS);
-    if (browserLocation.equals("local")) {
-      driver.manage().window().maximize();
-    }
-    return driver;
-  }
-
-  private static WebDriver chooseDriver(DesiredCapabilities capabilities) {
-    switch (driverName.toLowerCase()) {
-      case "phantomjs":
-        try {
-          driver = new PhantomJSDriver(capabilities);
-        } catch (Exception e) {
-          logger.error(e.getMessage());
-        }
-        return driver;
-      case "chrome":
-        ChromeOptions chromeOptions = new ChromeOptions();
-        if (Boolean.valueOf(isHeadless)) {
-          chromeOptions.addArguments("--headless");
-        }
-        try {
-          driver = new ChromeDriver(chromeOptions);
-        } catch (Exception e) {
-          logger.error(e.getMessage());
-        }
-        return driver;
-      default:
-        FirefoxOptions options = new FirefoxOptions();
-        if (Boolean.valueOf(isHeadless)) {
-          options.addArguments("-headless", "-safe-mode");
-        }
-        try {
-          driver = new FirefoxDriver(options);
-        } catch (Exception e) {
-          logger.error(e.getMessage());
+    private static WebDriver manageDriver(WebDriver driver) {
+        driver.manage().timeouts().setScriptTimeout(DEFAULT_WAIT, TimeUnit.SECONDS);
+        if (browserLocation.equals("local")) {
+            driver.manage().window().maximize();
         }
         return driver;
     }
-  }
 
-  private static DesiredCapabilities setDesiredCapabilities() {
-    DesiredCapabilities capabilities;
-    if (driverName.toLowerCase().contains("chrome")) {
-      capabilities = DesiredCapabilities.chrome();
-    } else {
-      capabilities = DesiredCapabilities.firefox();
+    private static WebDriver chooseDriver(DesiredCapabilities capabilities) {
+        switch (driverName.toLowerCase()) {
+            case "phantomjs":
+                try {
+                    driver = new PhantomJSDriver(capabilities);
+                } catch (Exception e) {
+                    logger.error(e.getMessage());
+                }
+                return driver;
+            case "chrome":
+                ChromeOptions chromeOptions = new ChromeOptions();
+                if (Boolean.valueOf(isHeadless)) {
+                    chromeOptions.addArguments("--headless");
+                }
+                try {
+                    driver = new ChromeDriver(chromeOptions);
+                } catch (Exception e) {
+                    logger.error(e.getMessage());
+                }
+                return driver;
+            default:
+                FirefoxOptions options = new FirefoxOptions();
+                if (Boolean.valueOf(isHeadless)) {
+                    options.addArguments("-headless", "-safe-mode");
+                }
+                try {
+                    //comment the below for full firefox Selenium
+                    disableFireFoxLogging();
+                    driver = new FirefoxDriver(options);
+                } catch (Exception e) {
+                    logger.error(e.getMessage());
+                }
+                return driver;
+        }
     }
 
-    capabilities.setJavascriptEnabled(true);
-    capabilities.setCapability("takesScreenshot", true);
-    return capabilities;
-  }
-
-  public static void closeDriver() {
-    if (!ObjectUtils.isEmpty(driver)) {
-      try {
-        driver.manage().deleteAllCookies();
-        driver.close();
-        driver.quit();
-      } catch (NoSuchMethodError nsme) {
-        // in case quit fails
-        logger.error("DriverConfigs.closeDriver: NoSuchMethodError", nsme);
-        nsme.printStackTrace();
-      } catch (NoSuchSessionException nsse) { // in case close fails
-        logger.error("DriverConfigs.closeDriver: NoSuchSessionException", nsse);
-        nsse.printStackTrace();
-      } catch (SessionNotCreatedException snce) {
-        logger.error("DriverConfigs.closeDriver: SessionNotCreatedException", snce);
-        snce.printStackTrace();
-      } // in case close fails
-      driver = null;
-    } else {
-      logger.warn("DriverConfigs.closeDriver Driver already closed...");
+    private static void disableFireFoxLogging() {
+        System.setProperty(FirefoxDriver.SystemProperty.DRIVER_USE_MARIONETTE, "true");
+        System.setProperty(FirefoxDriver.SystemProperty.BROWSER_LOGFILE, "/dev/null");
     }
-  }
+
+    private static DesiredCapabilities setDesiredCapabilities() {
+        DesiredCapabilities capabilities;
+        if (driverName.toLowerCase().contains("chrome")) {
+            capabilities = DesiredCapabilities.chrome();
+        } else {
+            capabilities = DesiredCapabilities.firefox();
+        }
+
+        capabilities.setJavascriptEnabled(true);
+        capabilities.setCapability("takesScreenshot", true);
+        return capabilities;
+    }
+
+    public static void closeDriver() {
+        if (!ObjectUtils.isEmpty(driver)) {
+            try {
+                driver.quit();
+            } catch (NoSuchMethodError nsme) {
+                // in case quit fails
+                logger.error("DriverConfigs.closeDriver: NoSuchMethodError", nsme);
+                nsme.printStackTrace();
+            } catch (NoSuchSessionException nsse) { // in case close fails
+                logger.error("DriverConfigs.closeDriver: NoSuchSessionException", nsse);
+                nsse.printStackTrace();
+            } catch (SessionNotCreatedException snce) {
+                logger.error("DriverConfigs.closeDriver: SessionNotCreatedException", snce);
+                snce.printStackTrace();
+            } // in case close fails
+            driver = null;
+        } else {
+            logger.warn("DriverConfigs.closeDriver Driver already closed...");
+        }
+    }
 }
